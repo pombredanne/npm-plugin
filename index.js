8678e0b1380721be2eb4df3cae0da30c23321456
doronzzz <gourdoron@gmail.com>
2014-11-09 19:50:49 +0200

    nocl

--- index.js
+++ index.js
@@ -116,11 +116,11 @@ var postJson = function(){
 	  var post_req = http.request(post_options, function(res) {
 	      res.setEncoding('utf8');
 	      res.on('data', function (chunk) {
-	          console.log('Response: ' + chunk);
+	          cli.ok('Response: ' + chunk);
 	          if(chunk.status == 1){
-	          	buildCallback();
+	          		buildCallback();
 	          }else{
-	          	cli.error('Build failed due to bad request');
+	          	//cli.error('Build failed due to bad request');
 	          }
 	      });
 	  });
