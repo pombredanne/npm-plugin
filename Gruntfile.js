d7326dd7683a806557911ba3cf224129d914b859
doronzzz <gourdoron@gmail.com>
2015-01-21 21:27:08 +0200

    grunt builder

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+// Generated on 2014-12-22 using generator-angular 0.10.0
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {// Project configuration.
+
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-file-append');
+
+
+grunt.initConfig({
+  uglify: {
+    my_target: {
+      files: {
+        'bin/whitesource': ['bin/whitesource.src']
+      }
+    }
+  },
+
+  file_append: {
+    default_options: {
+      files: [
+        function() {
+          return {
+            preappend: "\n/*\n//@ sourceMappingURL=testing2.js.map\n*/",
+            input: 'bin/whitesource_.js',
+            output:'bin/whitesource_2.js'
+          };
+        }
+      ]
+    }
+  }
+
+});
+
+
+  grunt.registerTask('default', [
+    //'newer:jshint',
+    'uglify',
+    /*'file_append:default_options'*/
+  ]);
+};
